39daa7d5c62b0928133ed43f61a2832d916751be
Chris Maguire <cwmaguire@gmail.com>
2016-08-06 01:10:13 -0700

    Print out all the chunks
    tEXt chunks are printed out with the 0 byte replaced by a semi-colon and
    without chunk info.
    
    All other chunks are printed out with a maximum of 100 bytes of data as
    well as the chunk info like data length, header type, whether or not
    this is an "extra" or "ancillary" chunk, etc.

--- png.erl
+++ png.erl
@@ -5,9 +5,35 @@
 decode(Path) ->
     {ok, Data} = file:read_file(Path),
     <<A:8, B:8, C:8, D:8, E:8, F:8, G:8, H:8, Rest/binary>> = Data,
-    io:format("~p, ~p, ~p, ~p, ~p, ~p, ~p, ~p~n", [A, B, C, D, E, F, G, H]),
-    <<ChunkLength1:32/integer, Chunk1Type:4/binary, Rest2/binary>> = Rest,
-    io:format("Chunk 1 length: ~p, Chunk 1 type: ~p~n", [ChunkLength1, Chunk1Type]),
-    <<Chunk1Data:ChunkLength1/binary, CRC1:4/binary, _Rest3/binary>> = Rest2,
-    io:format("Chunk 1 data: ~p~n", [Chunk1Data]),
-    io:format("Chunk 1 CRC: ~p~n", [CRC1]).
+    io:format("Header: ~p, ~p, ~p, ~p, ~p, ~p, ~p, ~p~n~n", [A, B, C, D, E, F, G, H]),
+    read_chunks(Rest).
+
+read_chunks(<<>>) ->
+    ok;
+read_chunks(<<ChunkLength:32/integer,
+              "tEXt",
+              Data:ChunkLength/binary,
+              _CRC:4/binary,
+              Rest/binary>>) ->
+    io:format("tEXt: ~p~n", [binary:replace(Data, <<0>>, <<":">>)]),
+    read_chunks(Rest);
+read_chunks(<<ChunkLength:32/integer,
+              ChunkType1:8/integer, ChunkTypeRest:3/binary,
+              Data:ChunkLength/binary,
+              CRC:4/binary,
+              Rest/binary>>) ->
+    IsAncillary = ChunkType1 band 32 == 32,
+    SampleSize = min(ChunkLength, 100),
+
+    <<Sample:SampleSize/binary, _/binary>> = Data,
+    io:format("Chunk length: ~p~n"
+              "Chunk Type: ~p~n"
+              "Ancillary? ~p~n"
+              "Chunk CRC: ~p~n"
+              "Chunk Data Sample: ~p~n~n",
+              [ChunkLength,
+               <<ChunkType1/integer, ChunkTypeRest/binary>>,
+               IsAncillary,
+               CRC,
+               Sample]),
+    read_chunks(Rest).
