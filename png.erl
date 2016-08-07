352b9dfe7777ab64742bca3889bf52ece91012cd
Chris Maguire <cwmaguire@gmail.com>
2016-08-06 17:59:42 -0700

    Display IHDR details, add safety valve
    Display the width, height and other details of the image from the IHDR
    chunk.
    
    Provide wildcard match in case we screw up the chunks and don't have any
    matches in read_chunks.

--- png.erl
+++ png.erl
@@ -2,14 +2,36 @@
 
 -export([decode/1]).
 
+-define(MAX_SAMPLE_SIZE, 10).
+
 decode(Path) ->
     {ok, Data} = file:read_file(Path),
     <<A:8, B:8, C:8, D:8, E:8, F:8, G:8, H:8, Rest/binary>> = Data,
-    io:format("Header: ~p, ~p, ~p, ~p, ~p, ~p, ~p, ~p~n~n", [A, B, C, D, E, F, G, H]),
+    io:format("Preamble: ~p, ~p, ~p, ~p, ~p, ~p, ~p, ~p~n~n", [A, B, C, D, E, F, G, H]),
     read_chunks(Rest).
 
 read_chunks(<<>>) ->
     ok;
+read_chunks(<<13:32,
+              "IHDR",
+              Width:32/integer,
+              Height:32/integer,
+              BitDepth:8/integer,
+              ColorType:8/integer,
+              Compression:8/integer,
+              Filter:8/integer,
+              Interlace:8/integer,
+              _CRC:4/binary,
+              Rest/binary>>) ->
+    io:format("IHDR~n\tW: ~p, H: ~p~n\t"
+              "Bit depth: ~p, Color type: ~p~n\t"
+              "Compression Method: ~p~n\t"
+              "Filter type: ~p~n\t"
+              "Interlace Method: ~p~n~n",
+              [Width, Height, BitDepth,
+               ColorType, Compression,
+               Filter, Interlace]),
+    read_chunks(Rest);
 read_chunks(<<ChunkLength:32/integer,
               "tEXt",
               Data:ChunkLength/binary,
@@ -18,12 +40,21 @@ read_chunks(<<ChunkLength:32/integer,
     io:format("tEXt: ~p~n", [binary:replace(Data, <<0>>, <<":">>)]),
     read_chunks(Rest);
 read_chunks(<<ChunkLength:32/integer,
+              "IDAT",
+              Data:ChunkLength/binary,
+              _CRC:4/binary,
+              Rest/binary>>) ->
+    io:format("IDAT:~p, ", [size(Data)]),
+    read_chunks(Rest);
+read_chunks(<<ChunkLength:32/integer,
               ChunkType1:8/integer, ChunkTypeRest:3/binary,
               Data:ChunkLength/binary,
               CRC:4/binary,
               Rest/binary>>) ->
+
+    ChunkTypeFull = <<ChunkType1, ChunkTypeRest/binary>>,
     IsAncillary = ChunkType1 band 32 == 32,
-    SampleSize = min(ChunkLength, 100),
+    SampleSize = min(ChunkLength, ?MAX_SAMPLE_SIZE),
 
     <<Sample:SampleSize/binary, _/binary>> = Data,
     io:format("Chunk length: ~p~n"
@@ -32,8 +63,10 @@ read_chunks(<<ChunkLength:32/integer,
               "Chunk CRC: ~p~n"
               "Chunk Data Sample: ~p~n~n",
               [ChunkLength,
-               <<ChunkType1/integer, ChunkTypeRest/binary>>,
+               ChunkTypeFull,
                IsAncillary,
                CRC,
                Sample]),
-    read_chunks(Rest).
+    read_chunks(Rest);
+read_chunks(_Data) ->
+    io:format("Unrecognized data, decode failed~n").
