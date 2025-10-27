ec25616c8158cc1ab0062b4efff0dea62b1071d4
Chris Maguire <cwmaguire@protonmail.com>
2025-10-26 18:11:40 -0700

    Testing out case statement var shadowing

--- case_shadow.erl
+++ case_shadow.erl
@@ -1,6 +1,17 @@
 -module(case_shadow).
 
 -export([a/2]).
+-export([b/1]).
+-export([c/1]).
+
+%% NOTE: as of 2025-01-18 I have case shadowing warnings disabled
+%% for erlang_ls: ~/.config/erlang_ls/erlang_ls.config
+%%
+%% diagnostics:
+%%   enabled:
+%%     - crossref
+%%   disabled:
+%%     - bound_var_in_pattern   <------
 
 a(Y, X) ->
     case Y of
@@ -10,4 +21,20 @@ a(Y, X) ->
             {x, 1};
         W ->
             {not_x, W}
+    end.
+
+b(Y) ->
+    X = Y,
+    case Y of
+        X ->
+            ok
+    end.
+
+c(Y) ->
+    X = a,
+    case Y of
+        X ->
+            ok;
+        _ ->
+            no
     end.
